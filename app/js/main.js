fdb1fb1205f58f2a4876d9a6900e714c00e9864c
Noli29 <noli.pogorelova@gmail.com>
2018-03-06 11:44:58 +0200

    update

--- app/js/main.js
+++ app/js/main.js
@@ -1,33 +1,57 @@
+//Aliases
+var Application = PIXI.Application,
+    loader = PIXI.loader,
+    TextureCache = PIXI.utils.TextureCache,
+    Rectangle = PIXI.Rectangle,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite;
+
 //Create a Pixi Application
-var app = new PIXI.Application({
-        width: 256,         // default: 800
-        height: 256,        // default: 600
-        antialias: true,    // default: false
-        transparent: false, // default: false
-        resolution: 1,      // default: 1
+var app = new Application({
+        antialias: true,
+        transparent: false,
+        resolution: 1,
         backgroundColor: 0x061639
     }
-)
-
+);
 app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
 app.renderer.autoResize = true;
 app.renderer.resize(window.innerWidth, window.innerHeight);
-
+var stage = new PIXI.Container();
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
 
 //load an image and run the `setup` function when it's done
-PIXI.loader
-    .add("images/cat.png")
+
+loader
+    .add("../app/images/food.png")
     .load(setup);
 
 //This `setup` function will run when the image has loaded
 function setup() {
 
-    //Create the cat sprite
-    let cat = new PIXI.Sprite(PIXI.loader.resources["images/cat.png"].texture);
+    //Create the `tileset` sprite from the texture
+    var texture = TextureCache["../app/images/food.png"];
 
-    //Add the cat to the stage
-    app.stage.addChild(cat);
+    //Create a rectangle object that defines the position and
+    //size of the sub-image you want to extract from the texture
+    //(`Rectangle` is an alias for `PIXI.Rectangle`)
+    var rectangle = new Rectangle(110, 110, 13, 13);
+
+    //Tell the texture to use that rectangular section
+    texture.frame = rectangle;
+
+    //Create the sprite from the texture
+    var rocket = new Sprite(texture);
+
+    //Position the rocket sprite on the canvas
+    rocket.x = 32;
+    rocket.y = 32;
+
+    //Add the rocket to the stage
+    app.stage.addChild(rocket);
+
+    //Render the stage
+    app.renderer.render(stage);
 }
